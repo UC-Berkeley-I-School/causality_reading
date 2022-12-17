7a8e56bedc0ae30a5f85bf491c16a4eab55b2d8b
tbayer <tbayer@users.noreply.github.com>
2022-12-17 06:13:41 +0000

    causal_forest.R: Autoinstall packages, note on data provenance

--- code/causal_forest.R
+++ code/causal_forest.R
@@ -1,6 +1,26 @@
-library(data.table)
+# Load Libraries and install them if needed
+
+# packages from GitHub:
+if (!"causalToolbox" %in% installed.packages()) {
+  if (!require("devtools")) install.packages("devtools")
+  devtools::install_github("forestry-labs/causalToolbox")
+  }
 library(causalToolbox)
 
+# CRAN packages:
+list.of.packages <- c("tidyverse", "ggplot2", "data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
+lapply(list.of.packages, require, character.only = TRUE)
+
+
+
+# data via Gerber and Green, "Field Experiments":
+# "Rind and Bordia studied the tipping behavior of lunchtime patrons of an “upscale 
+# Philadelphia restaurant” who were randomly assigned to four experimental groups.
+# One factor was server sex (male or female), and a second factor was whether the 
+# server draws a “happy face” on the back of the bill presented to customers.
+# Download the data located at http://isps.research.yale.edu/FEDAI "
 d <- fread("http://hdl.handle.net/10079/cd6be01a-a827-4312-a2fa-74329ce7f96d")
 
 X_RF(
